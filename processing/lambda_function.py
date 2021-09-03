82fa3cb86c32b2b95fd3a477a962b6d1c81b15d6
gASK13 <gask13@gmail.com>
2021-09-04 00:08:25 +0200

    Disable mentions in replies.

--- processing/lambda_function.py
+++ processing/lambda_function.py
@@ -13,7 +13,9 @@ def get_user(body):
 def post_response(content, body):
     # POST makes "NEW REPLY", PATCH makes "EDIT REPLY" (multiple windows vs one!!!)
     requests.patch(url = 'https://discord.com/api/v8/webhooks/' + os.environ['APP_ID'] + '/' + body.get('token') + "/messages/@original", headers = authConfig, data = {
-            "content" : content
+            "content" : content,
+            "allowed_mentions":
+                "parse":[]
         })
 
 def post_to_channel():
