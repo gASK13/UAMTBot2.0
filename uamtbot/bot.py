69333a002ffec11816cf66256eea9a8cfcbf781b
gASK13 <gask13@gmail.com>
2021-09-07 23:23:37 +0200

    Update edge case validation for indices.

--- uamtbot/bot.py
+++ uamtbot/bot.py
@@ -83,7 +83,7 @@ class UamtBot:
             return
         if options[0]['name'] == 'index':
             idx = int(options[0]['value']) - 1
-            if idx < 0 | idx >= len(notes['notes']):
+            if (idx < 0) | (idx >= len(notes['notes'])):
                 self.post_response("Sorry, you only have " + len(notes['notes']) + " ideas")
                 return
         elif options[0]['name'] == 'note':
