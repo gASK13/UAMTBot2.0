5af496268660c777d723e142707b1f121a55dc4f
gASK13 <gask13@gmail.com>
2021-09-30 23:41:54 +0200

    Fix slash AGE command skeleton.

--- uamtbot/commands/age.py
+++ uamtbot/commands/age.py
@@ -3,10 +3,14 @@ from uamtbot.commands.command import Command
 
 class AgeCommand(Command):
     @staticmethod
+    def ephemeral():
+        return True
+
+    @staticmethod
     def name():
         return 'age'
 
     @staticmethod
     def handle(body):
-        return super.generate_reply('Sorry, <@412352063125717002> did not get around to implementing it yet.')
+        return AgeCommand.generate_reply('Sorry, <@412352063125717002> did not get around to implementing it yet.')
 
